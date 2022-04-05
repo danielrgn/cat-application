920639d9e24899822d0399a01c59578962d7fc1e
Daniel <danielrgn@hotmail.com>
2022-04-05 01:24:36 -0300

    chore: add logs on the collect service class

--- collector/src/main/java/com/br/itau/cat/collector/application/service/CollectCatInfoService.java
+++ collector/src/main/java/com/br/itau/cat/collector/application/service/CollectCatInfoService.java
@@ -2,12 +2,14 @@ package com.br.itau.cat.collector.application.service;
 
 import com.br.itau.cat.collector.application.enums.CatCategoryEnum;
 import com.br.itau.cat.core.application.entity.Breed;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class CollectCatInfoService {
 
     @Autowired
@@ -17,11 +19,18 @@ public class CollectCatInfoService {
     private CatService catService;
 
     public void execute() {
+        log.info("Start collect breeds");
         final List<Breed> breeds = breedService.collectBreeds();
 
-        breeds.forEach(breed -> catService.saveCatsAndPictures(breed));
+        breeds.forEach(breed -> {
+            log.info("Start save cats and pictures by breed {}", breed);
+            catService.saveCatsAndPictures(breed);
+        });
 
+        log.info("Start save cats and pictures by hats");
         catService.saveCatsAndPictures(CatCategoryEnum.HATS);
+
+        log.info("Start save cats and pictures by sunglasses");
         catService.saveCatsAndPictures(CatCategoryEnum.SUNGLASSES);
     }
 }
